7f7e27af69988d77da4e2b20cd4b2f6f2d308e32
Mateus Lira <mateus.lira@kxp.consulting>
2023-04-21 09:50:35 -0300

    infra completed

--- infrastructure/akv/main.tf
+++ infrastructure/akv/main.tf
@@ -1,4 +1,5 @@
 data "azurerm_client_config" "current" {}
+data "azurerm_subscription" "primary" {}
 data "azurerm_kubernetes_cluster" "current" {
   name                = "k8s-${var.cname}"
   resource_group_name = "platform-${var.cname}"
@@ -16,7 +17,7 @@ resource "azurerm_key_vault" "main" {
   sku_name = "standard"
 
   access_policy {
-    tenant_id = data.azurerm_kubernetes_cluster.current.tenant_id
+    tenant_id = "4565bb37-9773-4d2e-80b6-398babdc2a33"
     object_id = data.azurerm_kubernetes_cluster.current.identity.0.principal_id
 
     key_permissions = [
@@ -52,4 +53,53 @@ resource "azurerm_key_vault" "main" {
       "Get"
     ]
   }
+
+   access_policy {
+    tenant_id = "4565bb37-9773-4d2e-80b6-398babdc2a33"
+    object_id = azurerm_role_assignment.argo.principal_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+      "Set",
+      "List",
+      "Delete"
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
+
+resource "random_password" "k8s" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
+}
+
+resource "azurerm_key_vault_secret" "k8s" {
+  name         = "k8s-${var.cname}" 
+  value        = random_password.k8s.result
+  key_vault_id = azurerm_key_vault.main.id
+}
+
+resource "azurerm_role_assignment" "argo" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.argo_sp.object_id
+}
+
+resource "azuread_application" "argo_app" {
+  display_name = "argo-${var.cname}"
+  owners       = [data.azurerm_client_config.current.object_id]
+}
+
+resource "azuread_service_principal" "argo_sp" {
+  application_id               = azuread_application.argo_app.application_id
+  app_role_assignment_required = false
+  owners                       = [data.azurerm_client_config.current.object_id]
 }
